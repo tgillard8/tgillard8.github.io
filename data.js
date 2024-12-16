02c9a2117b943d486fd15c9d389dca5f3b4169d7
tgillard8 <taviengill@gmail.com>
2024-12-16 13:45:51 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,16 @@
 d3.csv("NetflixOriginals.csv").then(function(data) {
-    // Parse the date and IMDB Score
-    const parseDate = d3.timeParse("%Y-%m-%d");
-    data.forEach(function(d) {
-        d["IMDB Score"] = +d["IMDB Score"];
-        d.Premiere = parseDate(d.Premiere);
-    });
+    // Group data by Genre and count the number of movies per Genre
+    const genreCounts = d3.rollup(data, v => v.length, d => d.Genre);
 
-    console.log("Loaded Data:", data); // Debug log to verify data loading
+    // Convert the grouped data into an array of objects
+    const genreData = Array.from(genreCounts, ([genre, count]) => ({ genre, count }));
+
+    console.log("Genre Data:", genreData); // Debug log to verify data grouping
 
     // Set up the SVG element
-    const margin = {top: 20, right: 30, bottom: 40, left: 40};
+    const margin = { top: 40, right: 30, bottom: 80, left: 60 };
     const width = 800 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    const height = 500 - margin.top - margin.bottom;
 
     const svg = d3.select("#chart")
         .append("svg")
@@ -21,32 +20,48 @@ d3.csv("NetflixOriginals.csv").then(function(data) {
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // Set up the scales
-    const x = d3.scaleTime()
-        .domain(d3.extent(data, d => d.Premiere))
-        .range([0, width]);
+    const x = d3.scaleBand()
+        .domain(genreData.map(d => d.genre))
+        .range([0, width])
+        .padding(0.2);
 
     const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d["IMDB Score"])])
+        .domain([0, d3.max(genreData, d => d.count)])
         .nice()
         .range([height, 0]);
 
     // Add the x-axis
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%Y")));
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end");
 
     // Add the y-axis
     svg.append("g")
         .call(d3.axisLeft(y));
 
-    // Add the scatter plot points
-    svg.selectAll("circle")
-        .data(data)
-        .enter().append("circle")
-        .attr("cx", d => x(d.Premiere))
-        .attr("cy", d => y(d["IMDB Score"]))
-        .attr("r", 3)
+    // Add the bars
+    svg.selectAll(".bar")
+        .data(genreData)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.genre))
+        .attr("y", d => y(d.count))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.count))
         .attr("fill", "steelblue");
+
+    // Add labels to the bars
+    svg.selectAll(".label")
+        .data(genreData)
+        .enter().append("text")
+        .attr("class", "label")
+        .attr("x", d => x(d.genre) + x.bandwidth() / 2)
+        .attr("y", d => y(d.count) - 5)
+        .attr("text-anchor", "middle")
+        .text(d => d.count);
 }).catch(function(error) {
     console.error('Error loading or parsing data:', error);
 });
